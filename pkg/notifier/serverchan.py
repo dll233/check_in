d9c2150274c562709b740668ff511090eb20f640
dll233 <49135636+dll233@users.noreply.github.com>
2022-01-30 12:28:30 +0800

    Update serverchan.py

--- pkg/notifier/serverchan.py
+++ pkg/notifier/serverchan.py
@@ -17,7 +17,8 @@ class Serverchan(object):
     def push(self, title, msg):
         print(f'检测到 "SERVERCHAN" 准备推送: {msg}')
 
-        url = 'https://sctapi.ftqq.com/{}.send'.format(self.token)
+        //url = 'https://sctapi.ftqq.com/{}.send'.format(self.token)
+        url = 'https://api2.pushdeer.com/{}.send'.format(self.token)
         # print(url)
 
         session = requests.Session()
